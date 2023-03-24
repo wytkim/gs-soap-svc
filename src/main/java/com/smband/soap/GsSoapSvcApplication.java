c59f217ddb0282926095ca2fe93fdcecc97c0e93
ytkim <ytkim@smband.co.kr>
2023-03-24 13:17:52 +0900

    초기 commit.

---
+++ src/main/java/com/smband/soap/GsSoapSvcApplication.java
@@ -0,0 +1,13 @@
+package com.smband.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSoapSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSoapSvcApplication.class, args);
+	}
+
+}
